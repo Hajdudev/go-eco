b3ab4c58bc09617935f9fe4c28b602b37459a0b4
Peter Hajdu <hajdu2024@gmail.com>
2025-02-23 17:28:47 +0100

    Updates Map component styles
    Increases the height and width of the Map component to 90% each to improve the visibility and user experience.

--- src/app/_components/Map.tsx
+++ src/app/_components/Map.tsx
@@ -10,7 +10,7 @@ export function Map() {
     <MapContainer
       center={position}
       zoom={13}
-      style={{ height: '80%', width: '50%' }}
+      style={{ height: '90%', width: '90%' }}
     >
       <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
       <Marker position={position}>
